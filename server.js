33f613280e2446233dac086a84e8d53ccf3cf32b
Shubham Bhola <shubhambhola87@gmail.com>
2021-07-12 11:33:31 +0530

    added heroku post build for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const mongoose = require('mongoose');
 const bodyParser= require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -27,8 +28,19 @@ const connectDB = async ()=> {
     }
 } 
 connectDB();
-
+//routes
 app.use('/api/items', items);
+
+//server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+//set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+    })
+
+
+}
 
 const port = process.env.PORT || 5000;
 
